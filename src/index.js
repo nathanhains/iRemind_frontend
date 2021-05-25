d4c331f26917a11ce47d384bf11c17a0920c8e41
nathan hains <nathanhains14@yahoo.com>
2021-05-25 16:36:33 -0400

    Reminder Comment removal.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// const endPoint = "http://localhost:3000/api/v1/reminders"
 let editMode = false
 const reminderListAdapter = new ReminderListAdapter("http://localhost:3000")
 
@@ -35,22 +34,6 @@ function addCreateReminderForm(){
     form.addEventListener("submit", (e) => createFormHandler(e))
 }
 
-// function getReminders(){
-//     //fetch
-//     fetch(endPoint)
-//     .then(response => response.json())
-//     //modify dom
-//     .then(reminders => {
-//         // backend serializer sets data into data key 
-//         reminders.data.forEach(reminder => {
-//             let newReminder = new Reminder(reminder, reminder.attributes)
-//             // calling the instance method
-//             document.querySelector("#reminder-container").innerHTML += newReminder.renderReminder()
-//         })
-
-//     })
-// }
-
 function createFormHandler(e){
     e.preventDefault()
     //e.target to get form
@@ -67,50 +50,8 @@ function postFetch(name, description, date, time, list_id){
 
     if(editMode){
         reminderListAdapter.editReminder(editMode, bodyData)
-        // fetch(endPoint+`/${editMode.dataset.id}`, {
-        //     method: "PATCH",
-        //     headers: {"Content-Type": "application/json"},
-        //     body: JSON.stringify(bodyData)
-        // })
-        // .then(response => response.json())
-        // .then(reminder => {
-        //     if(!reminder.status){
-        //         document.querySelector('#create-reminder-button').value = "CreateStore"
-        //         document.querySelector('form').reset()
-        //         editMode.children[0].innerText = reminder.name
-        //         editMode.children[1].innerText = reminder.description
-        //         editMode.children[2].innerText = reminder.date
-        //         editMode.children[3].innerText = reminder.time
-        //         editMode = false
-        //     }else{
-        //         alert(reminder.errors)
-        //     }
-        // })
-        // .catch(err => console.log(err))
     }else {
         reminderListAdapter.addReminder(bodyData)
-    // you only need to establish key names once if they are the same
-    // fetch(endPoint, {
-    //     method: "POST",
-    //     // json
-    //     headers: {"Content-Type": "application/json"},
-    //     // how to send back the data to the api
-    //     body: JSON.stringify(bodyData)
-    // })
-    // .then(response => response.json())
-    // .then(reminder => {
-    //     if(!reminder.status){
-    //         const reminderData = reminder.data
-    //         //render json response
-    //         let newReminder = new Reminder(reminderData, reminderData.attributes)
-    //         // calling the instance method
-    //         document.querySelector("#reminder-container").innerHTML += newReminder.renderReminder()
-    //         document.querySelector('form').reset()
-    //     }else{
-    //         alert(reminder.errors)
-    //     }
-    // })
-    // .catch(err=> console.log(err))
     }
 }
 
@@ -122,18 +63,6 @@ function handleEditDelete(e){
     const div = e.target.parentElement
     if (e.target.dataset.action === "delete"){
         reminderListAdapter.deleteReminder(div)
-        // fetch(endPoint+`/${div.dataset.id}`, {
-        //     method: "DELETE"
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //     if (data.message === "Successfully deleted"){
-        //         div.remove()
-        //     }else {
-        //         alert(data.message)
-        //     }
-        // })
-        // .catch(err => console.log(err))
     }else if(e.target.dataset.action === "edit") {
         editMode = div
 
